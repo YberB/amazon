4b5ff7cd332ba7102235b43fdd5dcb857673e150
yberC <yber.coronel@hotmail.com>
2023-12-12 10:15:29 -0500

    enter

--- js/amazon.js
+++ js/amazon.js
@@ -11,21 +11,24 @@ let spanProm3 = document.getElementById("promedio3");
 let spanProm4 = document.getElementById("promedio4");
 let spanProm5 = document.getElementById("promedio5");
 
-btngraficar.addEventListener("click", function(){
-    let sumaClificacion = (Number(calificacion1.value)+Number(calificacion2.value)+Number(calificacion3.value)+Number(calificacion4.value)+Number(calificacion5.value));
+btngraficar.addEventListener("click", function () {
+    let sumaClasificacion = Number(calificacion1.value) + Number(calificacion2.value) + Number(calificacion3.value) + Number(calificacion4.value) + Number(calificacion5.value);
 
-    //promedio
+    let promedio1 = (parseFloat(calificacion1.value) / sumaClasificacion) * 100;
+    let promedio2 = (parseFloat(calificacion2.value) / sumaClasificacion) * 100;
+    let promedio3 = (parseFloat(calificacion3.value) / sumaClasificacion) * 100;
+    let promedio4 = (parseFloat(calificacion4.value) / sumaClasificacion) * 100;
+    let promedio5 = (parseFloat(calificacion5.value) / sumaClasificacion) * 100;
 
-    let promedio1 = Math.round(parseFloat(calificacion1.value/sumaClificacion)*100);
-let promedio2 = Math.round(parseFloat(calificacion2.value/sumaClificacion)*100); 
-let promedio3 = Math.round(parseFloat(calificacion3.value/sumaClificacion)*100);
-let promedio4 = Math.round(parseFloat(calificacion4.value/sumaClificacion)*100);
-let promedio5 = Math.round(parseFloat(calificacion5.value/sumaClificacion)*100);
-
-spanProm1.innerHTML=promedio1
-spanProm2.innerHTML=promedio2
-spanProm3.innerHTML=promedio3
-spanProm4.innerHTML=promedio4
-spanProm5.innerHTML=promedio5
-
+    actualizarBarra(spanProm1, promedio1, "bar-1");
+    actualizarBarra(spanProm2, promedio2, "bar-2");
+    actualizarBarra(spanProm3, promedio3, "bar-3");
+    actualizarBarra(spanProm4, promedio4, "bar-4");
+    actualizarBarra(spanProm5, promedio5, "bar-5");
 });
+
+function actualizarBarra(elemento, porcentaje, claseBarra) {
+    porcentaje = Math.round(porcentaje); // Redondea el porcentaje
+    elemento.innerHTML = porcentaje + "%";
+    elemento.parentNode.querySelector("." + claseBarra).style.width = porcentaje + "%";
+}
